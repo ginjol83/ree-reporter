f89c23f0b572e460abd4326907f5e43f134e1214
agimenez122 <andresgimpe@gmail.com>
2024-02-28 22:53:58 +0100

    various fixes

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-import { 
-    getMercadoAhora 
-} from "./src/services/ree-service.service.js";
-
-// Importamos el módulo fs para poder escribir en archivos
-import fs from 'fs';
+import { getMercadoAhora, getBalance } from "./src/services/ree-service.service.js";
+import { writeXLS } from "./src/services/generate-excel-service.js";
 
 // Obtenemos los datos del mercado actual
-getMercadoAhora().then(result => {
-    // Convertimos el objeto JSON a una cadena
-    const dataString = JSON.stringify(result.data, null, 2); // El segundo argumento null y el tercero 2 son para formatear el JSON con indentación, haciéndolo más legible.
-
-    // Guardamos la cadena en un archivo
-    fs.writeFile('resultadoMercadoAhora.json', dataString, (err) => {
-        if (err) {
-            console.error('Hubo un error al guardar el archivo:', err);
-        } else {
-            console.log('El archivo fue guardado correctamente.');
-        }
-    });
+getMercadoAhora().then(marketNowResult => {
+    const mercadoAhora = marketNowResult.data
+    getBalance().then(balanceResult =>{
+        const balance = balanceResult.data
+        writeXLS(mercadoAhora, balance)
+    })
 });
+
+/*
+      type: 'Balance de energía eléctrica',
+      id: 'bal1',
+      attributes: {
+        title: 'Balance de energía eléctrica',
+        'last-update': '2024-01-09T13:29:33.000+01:00',
+        description: 'Balance eléctrico: asignación de unidades de producción según combustible principal. La producción neta de las instalaciones no renovables e hidráulicas UGH tiene descontados los consumos propios de las instalaciones. En dichos tipos de producción una generación negativa indica que la electricidad horaria consumida para los usos de las plantas excede la producción horaria de las instalaciones.'
+      },
+      meta: { 'cache-control': [Object] }
+    },
+    included: [
+      { type: 'Renovable', id: 'Renovable', attributes: [Object] },
+      { type: 'No-Renovable', id: 'No-Renovable', attributes: [Object] },
+      { type: 'Demanda', id: 'Demanda en b.c.', attributes: [Object] }
+*/
+
+
+
+/*[
+  {
+    type: 'Renovable',
+    id: 'Renovable',
+    attributes: {
+      title: 'Renovable',
+      'last-update': '2024-01-09T13:29:29.000+01:00',
+      description: null,
+      magnitude: null,
+      content: [Array]
+    }
+  },
+  {
+    type: 'No-Renovable',
+    id: 'No-Renovable',
+    attributes: {
+      title: 'No-Renovable',
+      'last-update': '2024-02-26T07:54:39.000+01:00',
+      description: null,
+      magnitude: null,
+      content: [Array]
+    }
+  },
+  {
+    type: 'Demanda',
+    id: 'Demanda en b.c.',
+    attributes: {
+      title: 'Demanda',
+      'last-update': '2024-02-26T07:54:41.000+01:00',
+      description: 'Demanda corregida: corregidos los efectos de temperatura y laboralidad.',
+      magnitude: null,
+      content: [Array]
+    }
+  }
+]*/
--- src/services/ree-service.service.js
+++ src/services/ree-service.service.js
@@ -3,9 +3,6 @@ import axios from "axios"
 var date = new Date()
 const currentYear = date.getFullYear()
 
-const token = "b55bcb9c3d55e34fdfdf46db50b36f2bcb7d1a063b40b2e24effdcf3caa9b57f"
-
-
 const balanceURL = "https://apidatos.ree.es/es/datos/balance/balance-electrico?start_date="+
         (currentYear-1)+"-"+(date.getMonth()+1)+"-"+date.getDate()+"T00:00&end_date="+currentYear+"-"+
         (date.getMonth()+1)+"-"+date.getDate()+"T23:59&time_trunc=day"
@@ -18,7 +15,6 @@ const mercadoMananaURL = "https://apidatos.ree.es/es/datos/mercados/precios-merc
         date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate()+1)+"T00:00&end_date="+date.getFullYear()+"-"+
         (date.getMonth()+1)+"-"+(date.getDate()+1)+"T23:59&time_trunc=hour"
 
-const mercadoAhoraApiReeURL = "https://api.preciodelaluz.org/v1/prices/all?zone=PCB"
 
 const mercadoAhoraURL = "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date="+
         date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate())+"T00:00&end_date="+date.getFullYear()+"-"+
@@ -28,11 +24,9 @@ const ireURL = "https://apidatos.ree.es/en/datos/demanda/ire-general?start_date=
        (date.getFullYear()-1)+"-"+(date.getMonth()+1)+"-"+(date.getDate())+"T00:00&end_date="+date.getFullYear()+"-"+
         (date.getMonth()+1)+"-"+(date.getDate())+"T23:59&time_trunc=month&geo_trunc=electric_system&geo_limit=peninsular&geo_ids=8741"
 
-const getBalanceAxios = () => axios.get(balanceURL,{}) 
+const getBalance = () => axios.get(balanceURL,{}) 
 
 const getIreIndex = () =>  axios.get(ireURL,{}) 
-
-const getMercadoAhoraApiRee = () => axios.get(mercadoAhoraApiReeURL,{})
 
 const getMercadoAhora = () => axios.get(mercadoAhoraURL,{})
 
@@ -48,4 +42,4 @@ const getMarketByDate = (date) => {
 
 const getMercadoManana = () => axios.get(mercadoMananaURL,{})
 
-export { getBalanceAxios, getIreIndex, getMercadoAhoraApiRee, getMercadoAhora, getMercadoPreciosRegulacionSecundaria, getMarketByDate, getMercadoManana }
+export { getBalance, getIreIndex, getMercadoAhora, getMercadoPreciosRegulacionSecundaria, getMarketByDate, getMercadoManana }
