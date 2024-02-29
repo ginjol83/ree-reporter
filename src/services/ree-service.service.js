0a936783ba51f71856c5d40f2f99fe90513d72db
agimenez122 <andresgimpe@gmail.com>
2024-02-29 23:47:54 +0100

    refactor ree-service

--- src/services/ree-service.service.js
+++ src/services/ree-service.service.js
@@ -1,45 +1,48 @@
 import axios from "axios"
 
 var date = new Date()
-const currentYear = date.getFullYear()
 
-const balanceURL = "https://apidatos.ree.es/es/datos/balance/balance-electrico?start_date="+
-        (currentYear-1)+"-"+(date.getMonth()+1)+"-"+date.getDate()+"T00:00&end_date="+currentYear+"-"+
-        (date.getMonth()+1)+"-"+date.getDate()+"T23:59&time_trunc=day"
+const lastYearNextMonth = `${(date.getFullYear()-1)}-${(date.getMonth()+1)}-${date.getDate()}`
+const currentMonth =      `${date.getFullYear()}-${(date.getMonth()+1)}-${date.getDate()}`
+const tomorrow =          `${date.getFullYear()}-${(date.getMonth()+1)}-${(date.getDate()+1)}`
+const nextMonth =         `${date.getFullYear()}-${(date.getMonth()+1)}-${(date.getDate())}`
+const lastYear =          `${(date.getFullYear()-1)}-${(date.getMonth()+1)}-${(date.getDate())}`
 
-const mercadoPreciosRegulacionSecundariaURL = "https://apidatos.ree.es/es/datos/mercados/energia-precios-regulacion-secundaria?start_date="+
-        date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate())+"T00:00&end_date="+date.getFullYear()+"-"+
-        (date.getMonth()+1)+"-"+(date.getDate())+"T23:59&time_trunc=year"
+const reeApiUrl = `https://apidatos.ree.es/es/datos/`
 
-const mercadoMananaURL = "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date="+
-        date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate()+1)+"T00:00&end_date="+date.getFullYear()+"-"+
-        (date.getMonth()+1)+"-"+(date.getDate()+1)+"T23:59&time_trunc=hour"
+const balanceURL = 
+        `${reeApiUrl}balance/balance-electrico?start_date=${lastYearNextMonth}T00:00&end_date=${currentMonth}T23:59&time_trunc=day`
 
+const mercadoPreciosRegulacionSecundariaURL = 
+        `${reeApiUrl}mercados/energia-precios-regulacion-secundaria?start_date=${nextMonth}T00:00&end_date=${nextMonth}T23:59&time_trunc=year`
 
-const mercadoAhoraURL = "https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date="+
-        date.getFullYear()+"-"+(date.getMonth()+1)+"-"+(date.getDate())+"T00:00&end_date="+date.getFullYear()+"-"+
-        (date.getMonth()+1)+"-"+(date.getDate())+"T23:59&time_trunc=hour"
+const mercadoMananaURL = 
+        `${reeApiUrl}mercados/precios-mercados-tiempo-real?start_date=${tomorrow}T00:00&end_date=${tomorrow}T23:59&time_trunc=hour`
 
-const ireURL = "https://apidatos.ree.es/en/datos/demanda/ire-general?start_date="+
-       (date.getFullYear()-1)+"-"+(date.getMonth()+1)+"-"+(date.getDate())+"T00:00&end_date="+date.getFullYear()+"-"+
-        (date.getMonth()+1)+"-"+(date.getDate())+"T23:59&time_trunc=month&geo_trunc=electric_system&geo_limit=peninsular&geo_ids=8741"
+const mercadoAhoraURL = 
+        `${reeApiUrl}mercados/precios-mercados-tiempo-real?start_date=${nextMonth}T00:00&end_date=${nextMonth}T23:59&time_trunc=hour`
+
+const ireURL = 
+        `${reeApiUrl}demanda/ire-general?start_date=${lastYear}T00:00&end_date=${nextMonth}T23:59&time_trunc=month&geo_trunc=electric_system&geo_limit=peninsular&geo_ids=8741`
+
+        
+const getIreIndex = () =>  axios.get(ireURL,{}) 
 
 const getBalance = () => axios.get(balanceURL,{}) 
 
-const getIreIndex = () =>  axios.get(ireURL,{}) 
-
 const getMercadoAhora = () => axios.get(mercadoAhoraURL,{})
+
+const getMercadoManana = () => axios.get(mercadoMananaURL,{})
 
 const getMercadoPreciosRegulacionSecundaria = () => axios.get(mercadoPreciosRegulacionSecundariaURL,{})
 
 const getMarketByDate = (date) => { 
-    let dateMoment = moment(date)
-    let url="https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date="+
-    dateMoment.year()+"-"+dateMoment.month()+"-"+(dateMoment.date())+"T00:00&end_date="+
-    dateMoment.year()+"-"+dateMoment.month()+"-"+(dateMoment.date())+"T23:59&time_trunc=hour"
+    const dateMoment = moment(date)
+    const url="https://apidatos.ree.es/es/datos/mercados/precios-mercados-tiempo-real?start_date="+
+    dateMoment.year()+"-"+dateMoment.month()+"-"+(dateMoment.date())+"T00:00&end_date="+dateMoment.year()+"-"+dateMoment.month()+"-"+(dateMoment.date())+"T23:59&time_trunc=hour"
     return axios.get(url,{}) 
   }
 
-const getMercadoManana = () => axios.get(mercadoMananaURL,{})
+
 
 export { getBalance, getIreIndex, getMercadoAhora, getMercadoPreciosRegulacionSecundaria, getMarketByDate, getMercadoManana }
